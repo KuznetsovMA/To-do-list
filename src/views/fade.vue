8bd0df1332c9deb7d4847da0c60bce136e4904e4
KuznetsovMA <jztenk@gmail.com>
2019-01-24 00:45:10 +0300

    style to SCSS blocks

--- src/views/fade.vue
+++ src/views/fade.vue
@@ -6,21 +6,3 @@
 export default {
 }
 </script>
-
-<style lang="scss">
-@import "../scss/vars.scss";
-
-.fade {
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 100;
-
-  width: 100%;
-  height: 100%;
-
-  background-color: $limeColor;
-  opacity: 0.8;
-}
-
-</style>
